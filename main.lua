bedf27b084a2d2d97ad4abbc685912519acdef61
Adam Hanny <ahkirbunkle@gmail.com>
2015-06-02 01:19:49 -0600

    added drawable and some test code

--- main.lua
+++ main.lua
@@ -1,9 +1,31 @@
+--classes
+local Drawable = require 'drawable'
+
+--globals
+local pc = nil
+
 function love.load()
+  pc = Drawable:new('graphics/purple.jpg', 100, 100, 0.3)
 end
 
 function love.update(dt)
+  local xVel = 0
+  local yVel = 0
+  if love.keyboard.isDown('up') then
+    yVel = yVel + (dt * -200)
+  end 
+  if love.keyboard.isDown('down') then
+    yVel = yVel + (dt * 200)
+  end 
+  if love.keyboard.isDown('right') then
+    xVel = xVel + (dt * 200)
+  end 
+  if love.keyboard.isDown('left') then
+    xVel = xVel + (dt * -200)
+  end 
+  pc:move(xVel, yVel)
 end
 
 function love.draw()
-  love.graphics.print('Jelly World!', 400, 300)
+  pc:draw()
 end
